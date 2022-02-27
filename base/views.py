5d7e052012f403c08e784d8f7cf7b19021e7b598
shah_arafat <shah@co.design>
2022-02-28 01:40:15 +0600

    basic db query

--- base/views.py
+++ base/views.py
@@ -1,33 +1,40 @@
 from django.shortcuts import render
 
+from .models import Room
+
 # Create your views here.
 
-rooms = [
-{
+# rooms = [
+# {
 
-  'id':1, 'name': 'Lets learn Python!'
-},
-{
-  'id':2, 'name': 'Design with me!'
-},
-{
-  'id':3, 'name': 'Frontend Developer!'
-},
-{
-  'id':4, 'name': 'Backend Developer!'
-},
-]
+#   'id':1, 'name': 'Lets learn Python!'
+# },
+# {
+#   'id':2, 'name': 'Design with me!'
+# },
+# {
+#   'id':3, 'name': 'Frontend Developer!'
+# },
+# {
+#   'id':4, 'name': 'Backend Developer!'
+# },
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for item in rooms:
-        if item['id'] == int(pk):
-            room = item
+    room = Room.objects.get(id=pk)
+    # for item in rooms:
+    #     if item['id'] == int(pk):
+    #         room = item
 
     context = {'room': room}
 
     return render(request, 'base/room.html',context)
+
+def createRoom(request):
+  context = {}
+  return render(request, 'base/room_form.html', context)
